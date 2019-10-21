2d3269572cc9dc7b2ab03574cd9cd9a9ea6dd4dd
guillaume <guillaume.hochet@gmail.com>
2019-10-21 16:27:13 +0200

    rewriting of everything

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import Scribus from './Scribus';
-import PopupRenderer from './Popup';
-
-const div = document.getElementById('scribus');
-Scribus.scribe(div, {
-  type: 'select',
-  renderer: new PopupRenderer(),
-});
