925bed9d8f0288c36a4dd11b16a7699b13908806
guillaume <guillaume.hochet@gmail.com>
2019-10-28 13:45:40 +0100

    add gh-pages publish script

---
+++ publish-docs.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+const { exec } = require('child_process');
+const path = require('path');
+const ghpages = require('gh-pages');
+
+console.log('Running build script');
+exec('npm run build-docs', (err) => {
+  if (err !== null) {
+    console.error(err);
+  } else {
+    console.log('Running gh-pages');
+    ghpages.publish(path.join(__dirname, 'docs', 'dist'), (err) => {
+      if (err) console.error(err);
+      else console.log('Done publishing docs website!');
+    });
+    console.log('Done!');
+  }
+});
